4d1dcda68cdadc39dabacf8b12b263f4d87f3512
Ben Slaughter <allsportster@gmail.com>
2017-04-28 10:51:27 -0600

    Moved all state to AppMain so that child components can use it for querying.

--- src/components/AppMain.js
+++ src/components/AppMain.js
@@ -6,23 +6,122 @@ import DynamicChart from './DynamicChart';
 
 class AppMain extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.handleSourceChange = this.handleSourceChange.bind(this);
+    this.handleCategoryChange = this.handleCategoryChange.bind(this);
+    this.handleCodeChange = this.handleCodeChange.bind(this);
+    this.handleTimeframeChange = this.handleTimeframeChange.bind(this);
+
+
+    this.state = {
+      sources: [],
+      timeframe: [Date.now() - (86400000 * 7), Date.now()],
+      categories: [],
+      codes: []
+    }
+  }
+
+  componentDidUpdate(nextProps, nextState) {
+    console.log("AppMain: Component Did Update");
+
+    if (this.state.sources.length != 0 &&
+      this.state.timeframe.length != 0 &&
+      this.state.categories.length != 0 &&
+      this.state.codes.length != 0) {
+
+      console.log(this.state.sources);
+      console.log(this.state.timeframe);
+      console.log(this.state.categories);
+      console.log(this.state.codes);
+
+
+    } else {
+      console.log("One of the states is empty. No need to fetch anything");
+    }
+  }
+
+  handleSourceChange(e) {
+
+    console.log("AppMain: Handling SOURCE change");
+    this.setState({
+      sources: e
+    });
+  }
+
+
+  handleCategoryChange(e) {
+
+    console.log("Handling CATEGORY change");
+    this.setState({
+      categories: e
+    });
+  }
+
+  handleCodeChange(e) {
+
+    console.log("AppMain: Handling CODE change");
+    this.setState({
+      codes: e
+    });
+  }
+
+  handleTimeframeChange(start, end) {
+
+    console.log("AppMain: Handling TIME change");
+
+    const timeArr = this.state.startEndTimes;
+
+    //If the start time is to be chagned
+    if (start) {
+      timeArr[0] = start;
+    }
+
+    //If the end time is to be changed
+    if (end) {
+      timeArr[1] = end;
+    }
+
+    console.log(timeArr);
+
+    //Make sure that the end time is after the start time
+    if (timeArr[0] < timeArr[1]) {
+
+      this.setState({
+        startEndTimes: timeArr
+      });
+
+    } else {
+      //Alert the user that they have to select an
+      //end that is later than the start time
+      console.log("AppMain: End is before start");
+      alert("End Time must be after Before Time");
+
+    }
+  }
+
+
   render() {
 
     return (
       <div className="container-fluid">
         <div className="row">
           <div className="col-md-3 sidebar">
-            <Sidebar />
+            <Sidebar sourceHandler={this.handleSourceChange} timeHandler={this.handleTimeframeChange}
+                     categoryHandler={this.handleCategoryChange} codeHandler={this.handleCodeChange}
+                     selectedSources={this.state.sources} startEndTimes={this.state.timeframe}
+                     categoryArray={this.state.categories} codeArray={this.state.codes}/>
           </div>
           <div className="col-md-5 map">
             <Map size={{width: '100%', height: '50vh'}}/>
             <MapLegend />
           </div>
           <div className="col-md-4 upperChart">
-            <DynamicChart />
+            {/*<DynamicChart />*/}
           </div>
           <div className="col-md-4 lowerChart">
-
+            {/*<DynamicChart />*/}
           </div>
         </div>
       </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,119 +9,11 @@ class Sidebar extends React.Component {
 
   constructor(props) {
     super(props);
-
-    this.handleSourceChange = this.handleSourceChange.bind(this);
-    this.handleCategoryChange = this.handleCategoryChange.bind(this);
-    this.handleCodeChange = this.handleCodeChange.bind(this);
-    this.handleTimeframeChange = this.handleTimeframeChange.bind(this);
-
-    this.state = {
-      sourceArray: [],
-      startEndTimes: [Date.now()-(86400000*7), Date.now()],
-      categoryArray: [],
-      codeArray: []
-    };
   }
-
-  handleSourceChange(e) {
-
-    console.log("Handling SOURCE change");
-    console.log(e);
-
-    this.setState({
-      sourceArray: e
-    });
-  }
-
-  handleCategoryChange(e) {
-
-    console.log("Handling CATEGORY change");
-    console.log(e);
-    this.setState({
-      categoryArray: e
-    });
-
-  }
-
-  handleCodeChange(e) {
-
-    console.log("Handling CODE change");
-    console.log(e);
-    this.setState({
-      codeArray: e
-    });
-
-  }
-
-  handleTimeframeChange(start, end) {
-
-    console.log("Handling TIME change");
-
-    const timeArr = this.state.startEndTimes;
-
-    //If the start time is to be chagned
-    if(start) {
-      timeArr[0] = start;
-    }
-
-    //If the end time is to be changed
-    if(end){
-      timeArr[1] = end;
-    }
-
-    console.log(timeArr);
-
-    //Make sure that the end time is after the start time
-    if(timeArr[0] < timeArr[1]) {
-
-      this.setState({
-        startEndTimes: timeArr
-      });
-
-    } else {
-      //Alert the user that they have to select an
-      //end that is later than the start time
-      console.log("End is before start");
-      alert("End Time must be after Before Time");
-
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-
-    console.log("New Props in Sidebar");
-
-    if (this.props != nextProps) {
-      console.log("Props did actually change");
-      this.props = nextProps;
-    }
-  }
-
-  componentWillUpdate(nextProps, nextState){
-
-    if(this.state != nextState){
-      console.log("New State Detected");
-
-      //TODO determine URL for SOLR call
-    }
-
-  }
-
-  componentDidMount() {
-    console.log("ComponentDidMount")
-  }
-  componentWillMount() {
-    console.log("ComponentWillMount")
-  }
-
-  componentWillUnmount() {
-    console.log("ComponentWillUnmount")
-  }5
 
   render() {
 
-
-    //Accordion panel of Target, Region, Resource, Objective
+    //Accordion panel of Sources, Timeframes, Categories, and Codes
     return (
       <div className="container-fluid" style={{textAlign: "left"}}>
         <div id="accordion1" className="panel-group">
@@ -134,7 +26,7 @@ class Sidebar extends React.Component {
             </div>
             <div id="collapseOne" className="panel-collapse collapse">
               <div className="panel-body">
-                <SourceSelectionPanel handler={this.handleSourceChange} selectedSources={this.state.sourceArray}/>
+                <SourceSelectionPanel handler={this.props.sourceHandler} selectedSources={this.props.selectedSources}/>
                 <div className="hero-circle">
                   <div className="hero-face"></div>
                 </div>
@@ -151,7 +43,8 @@ class Sidebar extends React.Component {
             </div>
             <div id="collapseTwo" className="panel-collapse collapse in">
               <div className="panel-body">
-                <TimeframeSelectionPanel handler={this.handleTimeframeChange} startTime={this.state.startEndTimes[0]} endTime={this.state.startEndTimes[1]}/>
+                <TimeframeSelectionPanel handler={this.props.timeHandler} startTime={this.props.startEndTimes[0]}
+                                         endTime={this.props.startEndTimes[1]}/>
               </div>
             </div>
           </div>
@@ -159,12 +52,13 @@ class Sidebar extends React.Component {
             <div className="panel-heading-blue">
               <h4 className="panel-title">
                 <a className="glyphicon glyphicon-th-large pad14" href="#collapseThree"></a>
-                <a className="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">Categories</a>
+                <a className="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion"
+                   href="#collapseThree">Categories</a>
               </h4>
             </div>
             <div id="collapseThree" className="panel-collapse collapse">
               <div className="panel-body">
-                <CategorySelectionPanel handler={this.handleCategoryChange} categories={this.state.categoryArray}/>
+                <CategorySelectionPanel handler={this.props.categoryHandler} categories={this.props.categoryArray}/>
               </div>
             </div>
           </div>
@@ -172,12 +66,13 @@ class Sidebar extends React.Component {
             <div className="panel-heading-blue">
               <h4 className="panel-title">
                 <a className="glyphicon glyphicon-barcode pad14" href="#collapseFour"></a>
-                <a className="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour">Codes</a>
+                <a className="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion"
+                   href="#collapseFour">Codes</a>
               </h4>
             </div>
             <div id="collapseFour" className="panel-collapse collapse">
               <div className="panel-body">
-                <CodeSelectionPanel handler={this.handleCodeChange} codes={this.state.codeArray}/>
+                <CodeSelectionPanel handler={this.props.codeHandler} codes={this.props.codeArray}/>
               </div>
             </div>
           </div>
